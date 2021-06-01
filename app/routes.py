1bdde7c6ba256d52a67bf6f8e609b96cc0408989
slavo3dev <slavo@slavo3.com>
2021-06-01 07:00:48 -0400

    simple api call to github

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ import logging
 
 import flask
 from flask import Response
+import requests
+
 
 app = flask.Flask("user_profiles_api")
 logger = flask.logging.create_logger(app)
@@ -15,6 +17,19 @@ def health_check():
     """
     app.logger.info("Health Check!")
     return Response("All Good!", status=200)
+
+
+
+@app.route("/api/github", methods=["GET"])
+def github_search():
+    r = requests.get("https://api.github.com/users/mailchimp")
+    data = r.json()
+    public_repos = "Public Repos: " + str(data["public_repos"])
+    followers= "Followers: " + str(data["followers"])
+
+    print("Data: ", public_repos)
+    app.logger.info("Github API")
+    return Response(public_repos + " | " + followers, status=200)
 
 
 
